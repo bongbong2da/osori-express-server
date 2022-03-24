b9e47b09c6dfb141d033d3b52c505f0994ba375d
bongbong2da <snk3981@gmail.com>
2022-03-24 21:46:07 +0900

    [Feat] Get Follows, Scraps API

--- src/routers/FollowRouter.ts
+++ src/routers/FollowRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { user } from '../models/user';
+import { trimNull } from '../utils/objectUtil';
 import sequelize from '../models';
 
 const FollowRouter = express.Router();
 const Follow = sequelize.follow;
+const User = sequelize.user;
 
 FollowRouter.post('/follow/:userId', async (req, res) => {
   try {
@@ -42,6 +43,52 @@ FollowRouter.delete('/follow/:userId', (req, res) => {
     console.log(e);
     res.status(e.code || 500);
     res.send(e.message || e);
+  }
+});
+
+FollowRouter.get('/follow/follower/:userId', async (req, res) => {
+  try {
+    const targetUserId = Number(req.params.userId);
+
+    const targetUser = await User.findOne({ where: { id: targetUserId } });
+    if (!targetUser) {
+      res.status(400);
+      res.send('사용자가 조회되지 않습니다');
+    } else {
+      const follows = await targetUser.getFolloweeFollows().then((res) => res);
+      const followers = await Promise.all(follows.map(async (follow) => {
+        const user = await follow.getFollowerUser();
+        return trimNull(user.get());
+      }));
+      res.send(followers);
+    }
+  } catch (e) {
+    console.log('err');
+    res.status(500);
+    res.send(e);
+  }
+});
+
+FollowRouter.get('/follow/following/:userId', async (req, res) => {
+  try {
+    const targetUserId = Number(req.params.userId);
+
+    const targetUser = await User.findOne({ where: { id: targetUserId } });
+    if (!targetUser) {
+      res.status(400);
+      res.send('사용자가 조회되지 않습니다');
+    } else {
+      const follows = await targetUser.getFolloweeFollows().then((res) => res);
+      const followers = await Promise.all(follows.map(async (follow) => {
+        const user = await follow.getFolloweeUser();
+        return trimNull(user.get());
+      }));
+      res.send(followers);
+    }
+  } catch (e) {
+    console.log('err');
+    res.status(500);
+    res.send(e);
   }
 });
 
---
+++ src/routers/ScrapRouter.ts
@@ -0,0 +1,78 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import _ from 'lodash';
+import { trimNull } from '../utils/objectUtil';
+import sequelize from '../models';
+
+const ScrapRouter = express.Router();
+const Scrap = sequelize.scrap;
+const User = sequelize.user;
+
+ScrapRouter.post('/scrap/:articleId', async (req, res) => {
+  try {
+    // @ts-ignore
+    const scrapper = jwt.decode((req.header('Authorization') as string).split(' ')[1]).user as user;
+    const articleId = Number(req.params.articleId);
+    const userExists = await User.findOne({ where: { id: scrapper.id } });
+    if (!userExists) {
+      throw { code: 500, message: '잘못된 사용자입니다' };
+    }
+    const exists = await userExists.getScraps();
+    if (_.find(exists, { articleId })) {
+      throw { code: 409, message: '이미 스크랩한 글입니다' };
+    } else {
+      Scrap.create({ userId: scrapper.id, articleId }).then((result) => {
+        res.send(result.get());
+      }).catch(() => {
+        throw { code: 500, message: 'server error' };
+      });
+    }
+  } catch (e : any) {
+    res.status(e.code || 500);
+    res.send(e.message || e);
+    console.log(e);
+  }
+});
+
+ScrapRouter.delete('/scrap/:articleId', (req, res) => {
+  try {
+    // @ts-ignore
+    const scrapper = jwt.decode((req.header('Authorization') as string).split(' ')[1]).user as user;
+    const articleId = Number(req.params.articleId);
+    Scrap.destroy({ where: { userId: scrapper.id, articleId } }).then((result) => {
+      switch (result) {
+        case 0:
+          res.send('스크랩하지 않은 글입니다');
+          break;
+        case 1:
+          res.send('스크랩을 해제했습니다');
+          break;
+        default:
+          res.status(500);
+          res.send('server error');
+      }
+    });
+  } catch (e : any) {
+    console.log(e);
+    res.status(e.code || 500);
+    res.send(e.message || e);
+  }
+});
+
+ScrapRouter.get('/scraps/:userId', async (req, res) => {
+  const targetUserId = Number(req.params.userId);
+  const targetUser = await User.findOne({ where: { id: targetUserId } });
+  if (!targetUser) {
+    res.status(500);
+    res.send('사용자 정보를 찾을 수 없습니다');
+  } else {
+    const scraps = await targetUser.getScraps();
+    const payload = await Promise.all(scraps.map(async (scrap) => {
+      const article = await scrap.getArticle();
+      return trimNull(article.get());
+    }));
+    res.send(payload);
+  }
+});
+
+export default ScrapRouter;
